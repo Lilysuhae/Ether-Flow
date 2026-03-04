d95b9f5f41fa251a6cfacfca086114060e11fc9e
Lilysuhae <asiemoltte@gmail.com>
2026-03-04 09:00:20 +0900

    0.5.10 진짜로 고친듯아마도

--- main.js
+++ main.js
@@ -28,6 +28,7 @@ function createWindow() {
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
+            backgroundThrottling: false,
         }
     });
 
